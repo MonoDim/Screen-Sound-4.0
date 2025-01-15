65c8f27525a2bbd8333d0716716efcd6d8148aaf
Gabriel Ramos <sanchesramosg@gmail.com>
2025-01-15 17:49:08 -0300

    Adicionar arquivos de projeto.

---
+++ Screen Sound 4.0/Modelos/Musica.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Principal;
+using System.Text.Json.Serialization;
+
+namespace Screen_Sound_4._0.Modelos;
+
+internal class Musica
+{
+    [JsonPropertyName("song")]
+    public  string? Nome { get; set; }
+    [JsonPropertyName("artist")]
+    public string? Artista { get; set; }
+    [JsonPropertyName("duration_ms")]
+    public int? Duracao { get; set; }
+    [JsonPropertyName("genre")]
+    public string? Genero { get; set; }
+
+    public void ExibirDetalhesDaMusica()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao / 1000}");
+        Console.WriteLine($"Genero: {Genero}");
+    }
+}
---
+++ Screen Sound 4.0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+using Screen_Sound_4._0.Modelos;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        musicas[1998].ExibirDetalhesDaMusica();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Sabe oq acontece? tem um pobrema: {ex.Message}");
+    }
+}
